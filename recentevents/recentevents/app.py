1789253903daebe8e3840564f43698f5192ab11d
Paul Osman <paul@knuth.local>
2009-10-07 07:29:21 -0400

    Adding a small webapp that wraps the FreshBooks event feed (so it's all on one domain).

---
+++ recentevents/recentevents/app.py
@@ -0,0 +1,14 @@
+from werkzeug import Response
+from redfox import WebApplication, get, post
+from urllib import urlopen
+
+class EventsFeed(WebApplication):
+    def __init__(self, global_config=None, feedurl, **local_config):
+        self.feedurl = feedurl
+    
+    @get('/')
+    def index(self, request):
+        feed = urlopen(self.feedurl)
+        response = Response(feed.read())
+        response.headers['Content-type'] = 'application/json'
+        return response
---
+++ recentevents/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='recentevents',
+    version='0.dev',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Werkzeug',
+        'redfox',
+        'setuptools'
+    ],
+    entry_points={
+        'paste.app_factory' : [
+            'recentevents=reventevents.app:EventsFeed'
+        ]
+    }
+)
