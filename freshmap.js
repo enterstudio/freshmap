83e5ab0251bc8bc296cf1da3c03dbc0e67bdbe7a
Paul Osman <paul@knuth.local>
2009-10-07 00:32:18 -0400

    Freshmap now uses google geocoding to translate a location name (i.e. Dublin, Ireland) into coordinates.

--- freshmap.js
+++ freshmap.js
@@ -3,9 +3,10 @@
 // panning we'll be doing (zoom out!).
 function FreshMap(mapelement) {
     var element = document.getElementById(mapelement);
+    this.geocoder = new google.maps.Geocoder();
     this.map = new google.maps.Map(element, {
-        zoom: 4,
-        center: this.getLocation(),
+        zoom: 3,
+        center: new google.maps.LatLng(0, 0),
         mapTypeId: google.maps.MapTypeId.TERRAIN,
         disableDefaultUI: true,
         scrollwheel: false
@@ -14,48 +15,56 @@ function FreshMap(mapelement) {
     this.infoWindow = null;
 }
 
-// pick a location at random and return it as a 2-element array
-// one element being latitude and one being longitude. 
+// pick a location at random and return it as an address string.
 FreshMap.prototype.getLocation = function() {
-    // mocked up location data. all in north america 
-    // and europe to ensure smooth panning
+    // mocked up location data. 
     this.locations = [
-        [43.67, -79.46], [53.46, -113.57], [49.29, -123.06], 
-        [41.54, -83.60], [39.81, -105.13], [32.80, -96.69], 
-        [30.33, -81.66], [53.35, -6.25],   [53.93, -1.68],  
-        [51.47, -0.1],   [41.93, 12.46]
+        'Dublin, Ireland', 'Ontario, Canada', 'Washington, United States',
+        'Georgia, United States', 'Wisconsin, United States',
+        'Kansas, United States', 'Colorado, United States',
+        'New Zealand', 'New South Wales, Australia', 'Singapore',
+        'New York, United States', 'California, United States'
     ];
-    var index  = Math.floor(Math.random() * this.locations.length);
-    var coords = this.locations[index];
-    return new google.maps.LatLng(coords[0], coords[1]);
+    return this.locations[Math.floor(Math.random() * this.locations.length)];
+}
+
+// reset marker and recenter map with the given latitude and longitude. 
+// create an info window with information about the given location
+FreshMap.prototype.createAndZoomToMarker = function(latlng, location) {
+    if (this.marker != null) {
+        this.marker.setMap(null);
+    }
+    this.marker = new google.maps.Marker({
+        position: latlng,
+        icon: 'freshlogo.png',
+        map: this.map,
+        title: 'Marker Title!'
+    });
+    if (this.infoWindow != null) 
+        this.infoWindow.close();
+    this.infoWindow = new google.maps.InfoWindow({
+        content: '<div class="eventInfo"><p>Someone in ' + location + 
+            ' tracked their time.</p></div>',
+        maxWidth: 300
+    });
+    this.infoWindow.open(this.map, this.marker);
+    this.map.panTo(latlng);
 }
 
 // start the event loop. every 5 seconds we create a new marker
 // and pan to it. the location of the marker will be based on 
 // the result from getLocation. 
 FreshMap.prototype.loop = function() {
-    var location = this.getLocation();
-    if (this.marker != null) {
-        this.marker.setMap(null);
-    }
-    if (this.infoWindow != null) {
-        this.infoWindow.close();
-    }
-    this.marker = new google.maps.Marker({
-        position: location,
-        icon: 'freshlogo.png',
-        map: this.map,
-        title: 'Marker! Woo!'
-    });
-    this.infoWindow = new google.maps.InfoWindow({
-       content: '<div class="eventInfo"><p>Someone in Western ' +
-        'Australia, Australia tracked their time.</p></div>',
-       maxWidth: 300
-    });
-    this.infoWindow.open(this.map, this.marker);
-    this.map.panTo(location);
     var c = this;
+    var location = this.getLocation();
+    this.geocoder.geocode({
+        'address' : location
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            c.createAndZoomToMarker(results[0].geometry.location, location)
+        }
+    });
     setTimeout(function() { 
         c.loop(); 
-    }, 5000);
+    }, 3000);
 }
