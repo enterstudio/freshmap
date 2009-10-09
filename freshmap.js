52449474d92536dd2a782765041b8c65a2233a60
Paul Osman <paul@knuth.local>
2009-10-08 20:39:42 -0400

    Cleaned up comments. did some housecleaning.

--- freshmap.js
+++ freshmap.js
@@ -1,26 +1,27 @@
+// shamelessly copy pasta'd from freshbooks.com
 Date.prototype.setISO8601 = function(dString){
-	var regexp = /(\d\d\d\d)(-)?(\d\d)(-)?(\d\d)(T)?(\d\d)(:)?(\d\d)(:)?(\d\d)(\.\d+)?(Z|([+-])(\d\d)(:)?(\d\d))/;
-	var d = dString.match(regexp);
-	var offset = 0;
+    var regexp = /(\d\d\d\d)(-)?(\d\d)(-)?(\d\d)(T)?(\d\d)(:)?(\d\d)(:)?(\d\d)(\.\d+)?(Z|([+-])(\d\d)(:)?(\d\d))/;
+    var d = dString.match(regexp);
+    var offset = 0;
 
-	this.setUTCDate(1);
-	this.setUTCFullYear(parseInt(d[1],10));
-	this.setUTCMonth(parseInt(d[3],10) - 1);
-	this.setUTCDate(parseInt(d[5],10));
-	this.setUTCHours(parseInt(d[7],10));
-	this.setUTCMinutes(parseInt(d[9],10));
-	this.setUTCSeconds(parseInt(d[11],10));
+    this.setUTCDate(1);
+    this.setUTCFullYear(parseInt(d[1],10));
+    this.setUTCMonth(parseInt(d[3],10) - 1);
+    this.setUTCDate(parseInt(d[5],10));
+    this.setUTCHours(parseInt(d[7],10));
+    this.setUTCMinutes(parseInt(d[9],10));
+    this.setUTCSeconds(parseInt(d[11],10));
 
-	if (d[12])
-		this.setUTCMilliseconds(parseFloat(d[12]) * 1000);
-	else
-		this.setUTCMilliseconds(0);
-	if (d[13] != 'Z') {
-		offset = (d[15] * 60) + parseInt(d[17],10);
-		offset *= ((d[14] == '-') ? -1 : 1);
-		this.setTime(this.getTime() - offset * 60 * 1000);
-	}
-	return this;
+    if (d[12])
+        this.setUTCMilliseconds(parseFloat(d[12]) * 1000); 
+    else
+        this.setUTCMilliseconds(0);
+    if (d[13] != 'Z') {
+        offset = (d[15] * 60) + parseInt(d[17],10);
+        offset *= ((d[14] == '-') ? -1 : 1);
+        this.setTime(this.getTime() - offset * 60 * 1000);
+    }
+    return this;
 };
 
 
@@ -41,27 +42,24 @@ function FreshMap(mapelement) {
     this.infoWindow = null;
     this.events = [];
     this.fetchEvents();
-}
+};
 
 // Try to extract the location string from the event description
 FreshMap.prototype.getLocationFromDescription = function(description) {
-
-    var start = "someone in ".length;    
+    var start = "someone in ".length; 
     var tokens = [
-        'tracked', 'sent a', 'created a', 'logged', 'got a'
+        'tracked', 'sent a', 'created a', 'logged', 'got'
     ];
-    
     for (var i = 0; i < tokens.length; i++) {
         var tokenIndex = description.indexOf(tokens[i]);
         if (tokenIndex > -1) {
             return description.substring(start, tokenIndex);
         }
     }
-    
     /* unknown event description, log it. */
     console.log("WARN: Unknown event: " + description);
     return null;
-}
+};
 
 // Given an event object, build a string representation in the form 
 // 'x second(s) ago, someone did something'
@@ -84,7 +82,7 @@ FreshMap.prototype.formatTimestampedEvent = function(event) {
     } else {
     	return ""; // stale event.
     }
-}
+};
 
 // Refresh the events queue.
 FreshMap.prototype.fetchEvents = function() {
@@ -92,7 +90,7 @@ FreshMap.prototype.fetchEvents = function() {
     $.getJSON('/recent-events', function(data) {
         c.events = data;
     });
-}
+};
 
 // Get the next event from the queue, refreshing if needed.
 FreshMap.prototype.nextEvent = function() {
@@ -102,9 +100,12 @@ FreshMap.prototype.nextEvent = function() {
     if (this.events.length > 0) 
         return this.events.shift();
     return null;
-}
+};
 
-// pick a location at random and return it as an address string.
+// pull the next event out of the event queue and cut it up 
+// into a location (i.e. Toronto, Ontario) and a description
+// (i.e. 32 Seconds ago someone in Toronto, Ontario did 
+// something). 
 FreshMap.prototype.getEvent = function() {
     var event = this.nextEvent();
     if (event == null) {
@@ -113,14 +114,14 @@ FreshMap.prototype.getEvent = function() {
     var description = this.formatTimestampedEvent(event);
     var location = this.getLocationFromDescription(event['description']);
     return {
-        'location' : location,
+        'location'    : location,
         'description' : description
     }
-}
+};
 
-// reset marker and recenter map with the given latitude and longitude. 
-// create an info window with information about the given location
-FreshMap.prototype.createMarker = function(latlng, location, description) {
+// reset marker and recenter map with the given latitude and 
+// longitude. create an info window with the event description.
+FreshMap.prototype.createMarker = function(latlng, description) {
     if (this.marker != null) {
         this.marker.setMap(null);
     }
@@ -138,11 +139,11 @@ FreshMap.prototype.createMarker = function(latlng, location, description) {
     });
     this.infoWindow.open(this.map, this.marker);
     this.map.panTo(latlng);
-}
+};
 
 // start the event loop. every 5 seconds we create a new marker
-// and pan to it. the location of the marker will be based on 
-// the result from getLocation. 
+// and pan to it. the location of the marker will be determined
+// by feeding the location string to google's geocoding API. 
 FreshMap.prototype.loop = function() {
     var c = this;
     var event = this.getEvent();
@@ -151,13 +152,11 @@ FreshMap.prototype.loop = function() {
             'address' : event.location
         }, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
-                c.createMarker(
-                    results[0].geometry.location, 
-                    event.location, event.description)
+                c.createMarker(results[0].geometry.location, event.description)
             }
         });
     }
     setTimeout(function() { 
         c.loop(); 
     }, 3000);
-}
+};
